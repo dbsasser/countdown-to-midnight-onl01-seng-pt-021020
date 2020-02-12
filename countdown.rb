5660294ab95bf23040ed573f2fe35efd53fd9005
Daniel Sasser <danielbsasser@gmail.com>
2020-02-12 00:12:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(number)
   
   while number > 0 
-    puts "#{number} SECOND(S)"
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
   
@@ -16,9 +16,11 @@ end
 def countdown_with_sleep(number)
 
   while number > 0 
-    puts "#{number} SECOND(S)"
+    puts "#{number} SECOND(S)!"
     number -= 1 
     sleep (1)
   end
+  
+  puts "HAPPY NEW YEAR!"
   
 end
