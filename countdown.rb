80af1052dacf0f59ac8bc0add42a434c7b91f2e1
Daniel Sasser <danielbsasser@gmail.com>
2020-02-12 00:13:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,7 @@ def countdown(number)
     number -= 1
   end
   
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
   
 end
 
@@ -21,6 +21,6 @@ def countdown_with_sleep(number)
     sleep (1)
   end
   
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
   
 end
